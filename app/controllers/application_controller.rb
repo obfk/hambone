e667bcce2c484fed9dc2acc6a394999792befa00
Pivotal Tracker <labs-tracker-team@pivotal.io>
2016-08-26 10:02:49 -0600

    utilize c3.css.min via CDN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "foobar", password: "pivotaltracker-hambone-2016"
-  
   protect_from_forgery with: :exception
 
   def index
